5a1a89d816641dbe96c1bc3293cb45d7c9ee1f62
gustavowebjs <gustavowebjs@gmail.com>
2021-06-25 10:16:36 -0300

    Inserts and get data from database

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,32 +1,136 @@
+import { useEffect } from "react";
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
 import logoImg from "../assets/img/logo.svg";
 import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 import "../styles/room.scss";
 
+type RoomParams = {
+  id: string;
+};
+
+type QuestionProps = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
 export function Room() {
+  const { user } = useAuth();
+
+  const [questions, setQuestions] = useState<QuestionProps[]>([]);
+  const [title, setTile] = useState("");
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.once("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setQuestions(parsedQuestions);
+      setTile(databaseRoom.title);
+    });
+  }, [roomId]);
+
+  const [newQuestion, setNewQuestion] = useState("");
+
+  async function handleSubmitQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === "") {
+      return;
+    }
+    if (!user) {
+      throw new Error("You must be logged!");
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+    setNewQuestion("");
+  }
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Logo Let Me Ask" />
 
-          <span>Código</span>
+          <RoomCode code={roomId} />
         </div>
       </header>
 
       <main className="content">
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>{title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
-        <form>
-          <textarea placeholder="O que você quer perguntar?"></textarea>
+        <form onSubmit={handleSubmitQuestion}>
+          <textarea
+            value={newQuestion}
+            onChange={(e) => setNewQuestion(e.target.value)}
+            placeholder="O que você quer perguntar?"
+          ></textarea>
 
           <div className="form-footer">
-            <span>
-              Para enviar uma pergunta, <button>Faça seu login</button>
-            </span>
-            <Button type="submit">Enviar Pergunta</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>Faça seu login</button>
+              </span>
+            )}
+
+            <Button disabled={!user} type="submit">
+              Enviar Pergunta
+            </Button>
           </div>
         </form>
       </main>
